d445c2ac5a051d1a0ccd8b686eb077862f56d779
Ezequiel Ferro <42ezequiel@gmail.com>
2023-05-21 11:54:13 -0300

    New classes

---
+++ Coche.java
@@ -0,0 +1,16 @@
+public class Coche{
+
+        public int puertas;
+
+        public static void main(){
+
+            Coche miCoche = new Coche();}
+            System.out.println( miCoche.addPuerta() );
+
+        }
+
+        public static void addPuerta(){
+            this.puerta++;
+        }
+
+}
---
+++ Suma.java
@@ -0,0 +1,13 @@
+public class Suma{
+
+    public static void main(){
+        //call function suma
+        suma( 1,2,3);
+    }
+
+    //implement method suma
+    public static int suma( int a, int b, int c){
+        return a + b + c;
+    }
+}
+
